f5ff778a855d997ecb64a7a688b5a80dd19e15a0
Kjeff24 <jeffreyarthur123@gmail.com>
2025-05-31 13:30:00 +0000

    feat: create main App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import PhotoDetail from './pages/PhotoDetail';
+import Upload from './pages/Upload';
+import Edit from './pages/Edit';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/photo/:id" element={<PhotoDetail />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/edit/:id" element={<Edit />} />
+        </Routes>
+        <ToastContainer 
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
